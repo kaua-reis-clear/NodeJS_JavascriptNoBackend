bb2e67f9a88b550e2d795b66dca5a8f07459b2b0
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-22 09:57:55 -0300

    NodeJS: using database to save data inserted with post, and listing it with get.

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,37 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+})
+
 module.exports = app => {
     app.get('/users', (req, res ) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({
-            users:[{
-                name: 'Hcode',
-                email: 'contato@hcode.com.br',
-                id: 1
-            }]
-        }));
+        db.find({}).sort({name: 1}).exec((err, users) => {
+            if(err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({
+                    users
+                })
+            }
+        });
     });
     
     app.post('/users', (req, res) => {
-        res.json(req.body);
+        db.insert(req.body, (err, user) => {
+            if (err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.status(200).json(user);
+            }
+        })
     })
 };
